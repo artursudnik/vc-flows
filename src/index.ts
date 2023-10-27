f0a20083268c7169d15b3b5aae278e6fd4faafc8
Artur Sudnik-Hrynkiewicz <artur.sudnik@gmail.com>
2023-10-27 14:56:50 +0200

    chore: refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { randomUUID } from 'node:crypto';
 import { IDC_BASE_URL, VC_BASE_URL } from './const';
-import { credExchSelfSignedCredTutorial } from './cred-exch-self-signed-cred-tutorial.function';
+import { credExchSelfSignedCredTutorial } from './lib';
 import { presentationDefinitionTutorial } from './presentation-definitions';
 
 const presentationDefinition = {
